5f8a2d96262ac07e2f31f8194df5670c62a963c9
Ioan Hadarean <hadareannelutu@yahoo.com>
2019-04-18 21:22:46 +0000

    Added validation for game over/user did not guess right

--- app.js
+++ app.js
@@ -11,17 +11,17 @@ GAME FUNCTION:
 let min = 1,
     max = 10,
     winningNum = 2,
-    guessesLeft =  3;
-    
+    guessesLeft = 3;
+
 // UI Elements
-const game =  document.getElementById('game'),
-      minNum = document.querySelector('.min-num'),
-      maxNum = document.querySelector('.max-num'),
-      guessBtn =  document.querySelector('#guess-btn'),
-      guessInput = document.querySelector('#guess-input'),
-      message = document.querySelector('.message');
-      
-    
+const game = document.getElementById('game'),
+    minNum = document.querySelector('.min-num'),
+    maxNum = document.querySelector('.max-num'),
+    guessBtn = document.querySelector('#guess-btn'),
+    guessInput = document.querySelector('#guess-input'),
+    message = document.querySelector('.message');
+
+
 // Assign UI min and max
 minNum.textContent = min;
 maxNum.textContent = max;
@@ -31,21 +31,49 @@ maxNum.textContent = max;
 guessBtn.addEventListener('click', function() {
     let guess = parseInt(guessInput.value);
     // Validate
-    if(isNaN(guess) || guess < min || guess > max) {
+    if (isNaN(guess) || guess < min || guess > max) {
         setMessage(`Please enter a number between ${min} and ${max}`, 'red');
     }
-    
+
     // Check if won
-    if(guess === winningNum){
+    if (guess === winningNum) {
+        // Player wins
+
         // Disable input
         guessInput.disabled = true;
         // Change border color
         guessInput.style.borderColor = 'green';
         // Set message
         setMessage(`${winningNum} is correct, YOU WIN!`, 'green');
-        
-    } else {
-        
+
+    }
+    else {
+
+        // Wrong number
+        guessesLeft -= 1;
+
+        if (guessesLeft === 0) {
+            //Player loses
+
+            // Disable input
+            guessInput.disabled = true;
+            // Change border color
+            guessInput.style.borderColor = 'red';
+            // Set message
+            setMessage(`Game Over, you lost. The correct number was ${winningNum}`, 'red');
+        }
+        else {
+            // Game continues - answer wrong
+            
+            // Change border color
+            guessInput.style.borderColor = 'red';
+            
+            // Clear input
+            guessInput.value = '';
+            
+            // Tell player it's the wrong number
+            setMessage(`${guess} is not correct, you have ${guessesLeft} guesses left`);
+        }
     }
 });
 
