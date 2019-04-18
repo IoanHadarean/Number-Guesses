a98317d4557293dac3de68401b660834fec539c0
Ioan Hadarean <hadareannelutu@yahoo.com>
2019-04-18 21:05:11 +0000

    Added listener for guess button/added error checks and message

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ GAME FUNCTION:
 
 // Game values
 let min = 1,
-    max = 9,
+    max = 10,
     winningNum = 2,
     guessesLeft =  3;
     
@@ -18,10 +18,25 @@ const game =  document.getElementById('game'),
       minNum = document.querySelector('.min-num'),
       maxNum = document.querySelector('.max-num'),
       guessBtn =  document.querySelector('#guess-btn'),
-      guessInput = document.querySelector('.#guess-input'),
+      guessInput = document.querySelector('#guess-input'),
       message = document.querySelector('.message');
       
     
 // Assign UI min and max
 minNum.textContent = min;
 maxNum.textContent = max;
+
+
+// Listen for guess
+guessBtn.addEventListener('click', function() {
+    let guess = parseInt(guessInput.value);
+    // Validate
+    if(isNaN(guess) || guess < min || guess > max) {
+        setMessage(`Please enter a number between ${min} and ${max}`);
+    }
+});
+
+// Set message
+function setMessage(msg) {
+    message.textContent = msg;
+}
